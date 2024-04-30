6f0eb93516576a734f31ee59d90e86809c1c68e2
unknown <estherinyang07@gmail.com>
2024-04-30 08:07:00 +0100

    styled all pages

---
+++ proactive-health-with-ai/app/about/page.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="p-20">
+      <div className="w-[60%] mx-auto rounded-sm">
+        {/* text-[#101532] */}
+
+        <div className="p-20 mt-7 border-l-4 border-l-[#1F2B6C] bg-[#d7e4fc] rounded-md">
+          <h2 className="text-3xl leading-loose tracking-wide text-[#101532] font-extrabold text-center">
+            Proactive Health With AI
+          </h2>
+          <h2 className="mt-5 mb-10 text-2xl leading-loose tracking-wide text-[#101532] font-normal text-center">
+            We are Professional AI Medical Assistant
+          </h2>
+          <p className="text-base leading-loose tracking-wide text-[#101532] font-normal text-center">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima
+            iusto nemo quaerat itaque laboriosam libero, eaque vitae culpa
+            aliquam sapiente quibusdam cumque cum placeat hic ducimus ut officia
+            dolorum? Et distinctio fugiat hic quasi omnis maxime unde autem, eum
+            explicabo harum incidunt, sapiente voluptates? Asperiores
+            voluptatibus ea voluptatum optio ratione deserunt? Amet porro
+            reprehenderit fugit adipisci voluptas expedita officiis laudantium
+            dolorum neque temporibus fuga libero commodi, voluptatem ducimus
+            sint laboriosam placeat esse at ad! Minima perspiciatis, delectus
+            debitis adipisci mollitia animi odit consequatur beatae quas
+            expedita repellendus impedit, ad eaque enim voluptas ut nihil
+            nesciunt architecto omnis ipsum non aut.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
