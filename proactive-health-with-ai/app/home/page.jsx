4d9af4d662f564c1df40cfede8b52e87c69c642c
GdglagosAiTeam7 <ctochukwu131@gmail.com>
2024-05-01 12:37:37 +0100

    testing image routes lives

--- proactive-health-with-ai/app/home/page.jsx
+++ proactive-health-with-ai/app/home/page.jsx
@@ -28,11 +28,12 @@ const handleClick = async (e) => {
   console.log(file)
   try {
     const newPost = await axios.post(
+      // "http://localhost:5000/api/upload-image",
       "https://proactive-health-ai.onrender.com/api/upload-image",
       {
         question,
         mimeType: file.type,
-        path: `out/${file.name}`,
+        path: `proactive-health-with-ai/public/${file.name}`,
         headers: {
           "Content-Type": "application/json",
         },
