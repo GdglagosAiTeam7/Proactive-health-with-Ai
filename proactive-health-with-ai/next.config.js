a99efbe10e6400775a02bc75daab1b24c7d755e5
GdglagosAiTeam7 <ctochukwu131@gmail.com>
2024-05-01 08:25:52 +0100

    reconfigured next config

--- proactive-health-with-ai/next.config.js
+++ proactive-health-with-ai/next.config.js
@@ -1,23 +1,31 @@
 module.exports = {
     async redirects() {
       return [
-        // Basic redirect: /about to /
         {
-          source: '/_not-found',
-          destination: '/home',
-          permanent: true, // 301 (Permanent) or 307 (Temporary)
-        },
-        // Wildcard path matching: /blog/:slug to /news/:slug
-        {
-          source: '/about',
-          destination: '/about',
-          permanent: true,
-        },
-        {
-          source: '/',
-          destination: '/home',
-          permanent: true, // 301 (Permanent) or 307 (Temporary)
+          source: '/:path*',
+          destination: '/',
         },
       ];
+      
     },
   };
+
+  // return [
+      //   // Basic redirect: /about to /
+      //   {
+      //     source: '/_not-found',
+      //     destination: '/home',
+      //     permanent: true, // 301 (Permanent) or 307 (Temporary)
+      //   },
+      //   // Wildcard path matching: /blog/:slug to /news/:slug
+      //   {
+      //     source: '/about',
+      //     destination: '/about',
+      //     permanent: true,
+      //   },
+      //   {
+      //     source: '/',
+      //     destination: '/home',
+      //     permanent: true, // 301 (Permanent) or 307 (Temporary)
+      //   },
+      // ];
